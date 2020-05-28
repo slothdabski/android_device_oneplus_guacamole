aed33b776d8ba6c0f7b3de8a1754bff5c31b5f07
slothdabski <allthatsleft169@gmail.com>
2020-05-28 10:23:11 -0500

    Let's bake some candy

--- candy_guacamole.mk
+++ candy_guacamole.mk
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from guacamole device
 $(call inherit-product, device/oneplus/guacamole/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Candy.
+$(call inherit-product, vendor/candy/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_guacamole
+PRODUCT_NAME := candy_guacamole
 PRODUCT_DEVICE := guacamole
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := OnePlus 7 Pro
@@ -37,3 +37,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="OnePlus7Pro-user 10 QKQ1.190716.003 1909110008 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+# Inherit gapps
+$(call inherit-product-if-exists, vendor/gapps/gapps.mk)
